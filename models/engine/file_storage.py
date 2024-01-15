200ef051b083b350d4e9e44eccce6174cca9c337
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-15 02:41:54 +0200

    [push] project AirBnB_clone_v2 task[5]

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            newDict = {}
+            for key, value in FileStorage.__objects.items():
+                if cls == type(value):
+                    newDict[key] = value
+            return newDict
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it is inside"""
+        if obj and obj in FileStorage.__objects.values():
+            key = f'{obj.__class__.__name__}.{obj.id}'
+            del FileStorage.__objects[key]
+            self.save()
