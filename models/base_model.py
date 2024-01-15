e3bcdcdb868f5d4d1def7766bb30facc03aa1fe4
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-15 03:24:42 +0200

    [push] project AirBnB_clone_v2 task[3,4]

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import models
 import uuid
 from datetime import datetime
+
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, time_format)
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(value, time_format)
+                elif key == "__class__":
+                    continue
+                else:
+                    self.__dict__[key] = value
+        else:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
