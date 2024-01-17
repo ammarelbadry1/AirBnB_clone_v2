d45486aa6ba68e1b687aca78190fac2035811d05
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-17 10:57:04 +0200

    fix problems

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,4 +69,11 @@ class DBStorage():
 
     def reload(self):
         """method create all tables in the database"""
+        from models.base_model import BaseModel, Base
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
         Base.metadata.create_all(self.__engine)
